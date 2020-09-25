1882306a6915862f2ea13c7c19624dd068b9a28f
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-09-25 23:48:53 +0530

    wireuschallenge day8

---
+++ Day_8.js
@@ -0,0 +1,3 @@
+var subjects = ["Data Structures", "Physics", "Chemistry", "Mathematics", "Biology", "English", "Hindi"];
+var query = window.prompt("Enter a number(0 <=number <=6): ");
+document.write(subjects[query]);
