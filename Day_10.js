3b0a9b017d624a592d3e8f3b74d3e0428eab712d
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-09-27 23:50:58 +0530

    wireuschallenge day10

---
+++ Day_10.js
@@ -0,0 +1,9 @@
+function callMom(){
+    var x = prompt("Are you hungry?");
+    if(x == "YES"){
+      document.write("I am hungry!");
+    }
+    else{
+        document.write("Don't call the function for fun!!!");
+    }
+}
