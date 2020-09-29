f3fb5616d7bde199cd35a1cffed60842a07368a2
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-09-29 23:39:49 +0530

    Adding code for Day12

---
+++ Day12.js
@@ -0,0 +1,10 @@
+function sendBatSignal(){
+    // var bat=document.getElementById('bat');
+    document.body.innerHTML="It is not who I am underneath but what I do defines me!"
+    var img=document.createElement('img');
+    img.src='bat.jpg';
+    img.width="500";
+    img.height="500";
+    img.style.display="block";
+    document.querySelector('body').appendChild(img);
+}
