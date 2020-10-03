180916a66d3605aede4919eeb06498f32e5ad18e
weirdrag08 <60457205+weirdrag08@users.noreply.github.com>
2020-10-03 22:12:37 +0530

    wireus challenge Day16

---
+++ Day16.js
@@ -0,0 +1,15 @@
+// var plant=document.getElementById("one");
+// var cls=
+// function grow(){
+//     document.getElementsById('myPlant').class='two';
+// }
+
+// function grow(){
+//     // plant.style.height="1000px";
+//     document.getElementsById('myPlant').removeClass().addClass('two');
+// }
+
+var g=document.getElementById("myPlant");
+function show(){
+    g.src="long-neck-giraffe.jpeg";
+}
